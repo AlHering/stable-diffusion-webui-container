6ab6cd4e4e3047a89021ee7d3ceae91d53c27c3c
AlHering <dev.hering@gmail.com>
2023-05-11 14:39:25 +0200

    Update run_webui.sh
    added header docs

--- run_webui.sh
+++ run_webui.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+#################################################
+# Access point for starting the webui
+# with standard commandline args
+#################################################
+
 cd "/stable-diffusion-webui-container/stable-diffusion-webui"
 export COMMANDLINE_ARGS="--allow-code --skip-install --xformers --device-id 0"
 bash webui.sh
