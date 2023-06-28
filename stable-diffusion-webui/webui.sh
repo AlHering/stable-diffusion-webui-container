ad607f254066c15bd8042f26a678918eaa150e77
Alexander Hering <dev.hering@gmail.com>
2023-06-28 14:30:57 +0200

    adjusted webui for containerization

--- stable-diffusion-webui/webui.sh
+++ stable-diffusion-webui/webui.sh
@@ -1,54 +1,22 @@
 #!/usr/bin/env bash
-#################################################
-# Please do not make any changes to this file,  #
-# change the variables in webui-user.sh instead #
-#################################################
-
-# If run from macOS, load defaults from webui-macos-env.sh
-if [[ "$OSTYPE" == "darwin"* ]]; then
-    if [[ -f webui-macos-env.sh ]]
-        then
-        source ./webui-macos-env.sh
-    fi
-fi
-
-# Read variables from webui-user.sh
-# shellcheck source=/dev/null
-if [[ -f webui-user.sh ]]
-then
-    source ./webui-user.sh
-fi
-
 # Set defaults
 # Install directory without trailing slash
-if [[ -z "${install_dir}" ]]
-then
-    install_dir="$(pwd)"
-fi
+install_dir="/stable-diffusion-webui-container"
 
-# Name of the subdirectory (defaults to stable-diffusion-webui)
-if [[ -z "${clone_dir}" ]]
-then
-    clone_dir="stable-diffusion-webui"
-fi
+# Name of the subdirectory
+clone_dir="stable-diffusion-webui"
 
 # python3 executable
-if [[ -z "${python_cmd}" ]]
-then
-    python_cmd="python3"
-fi
+python_cmd="python3"
 
 # git executable
-if [[ -z "${GIT}" ]]
-then
-    export GIT="git"
-fi
+export GIT="git"
 
 # python3 venv without trailing slash (defaults to ${install_dir}/${clone_dir}/venv)
-if [[ -z "${venv_dir}" ]]
-then
-    venv_dir="venv"
-fi
+venv_dir="/stable-diffusion-webui-container/venv"
+
+# install command for torch
+export TORCH_COMMAND="pip install --no-cache-dir torchvision==0.14.1 torchaudio==0.13.1 torch==1.13.1+cu117 --extra-index-url https://download.pytorch.org/whl/cu117"
 
 if [[ -z "${LAUNCH_SCRIPT}" ]]
 then
